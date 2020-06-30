9a8f1a2bce491fc05c56a8bb385fca85b04e9319
Johnny Rice <jrice@labact.com>
2020-06-30 01:11:38 -0400

    Provide challenge 05

--- script.js
+++ script.js
@@ -4,8 +4,14 @@
 const finalChallenge = ['What', 'game', 'do', 'these', 'functions', 'belong', 'to?']
 
 
-// Turn that array into a single string. Find the Array method and usage to make a sentence
-// Replace `whatGoesHere` with a method call with the appropriate param value(s)
-const finalQuestion = finalChallenge.whatGoesHere
+// Turn that array into a single string of "What game do these functions belong to?". 
+// Find the Array method and usage to make a sentence
+// Found on 
+// - https://www.w3schools.com/js/js_array_methods.asp
+// - https://javascript.info/array-methods
+// - https://blog.logrocket.com/understand-array-methods-by-implementing-them/
+// - ⬆️ Seriously go visit this last one. What a great read ⬆️ BRILLIANT!
+const finalQuestion = finalChallenge.join(' ')
 
+// Expect "What game do these functions belong to?"
 console.log({ finalQuestion })
