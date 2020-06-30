a27bb5573ee6a1c26a6f3bb1d2dd749515fb8f82
Johnny Rice <jrice@labact.com>
2020-06-30 00:01:32 -0400

    Provide inline instructions

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
-        // Set all variables to their base values
+        // @TODO 1) give this function a descriptive name
+        //       2) provide your "tweet" description of what the function does in your commit message
+        //
+        //        Let's make great commit messages:
+        //        -- remember to read https://chris.beams.io/posts/git-commit/
+        //
+        //        For now the key items from that post to apply are:
+        //        -- Capitalize the commit message
+        //        -- Do not end the message with a period
+        //        -- Use the imperative mood in the subject line
+        //        -- Your message completes this "If applied, this commit will" "Your commit message here"
         function nameThisFunction () {
 
             playerChoice = "";
