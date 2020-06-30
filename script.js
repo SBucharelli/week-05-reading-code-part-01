67d125b0df9d0f8b95a307440337d1f349e41da3
Johnny Rice <jrice@labact.com>
2020-06-29 23:49:47 -0400

    Provide question 02

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 
-        // Sets all the values on the board
-        function showChoicesAndWinner(playerChoice, computerChoice, winnerText) {
+        // @TODO 1) give this function a descriptive name
+        //       2) provide your "tweet" description of what the function does in your commit message
+        //
+        //        Let's make great commit messages:
+        //        -- remember to read https://chris.beams.io/posts/git-commit/
+        //
+        //        For now the key items from that post to apply are:
+        //        -- Capitalize the commit message
+        //        -- Do not end the message with a period
+        //        -- Use the imperative mood in the subject line
+        //        -- Your message completes this "If applied, this commit will" "Your commit message here"
+        function nameThisFunction(playerChoice, computerChoice, winnerText) {
 
             $('.player-choice').text(playerChoice);
             $('.computer-choice').text(computerChoice);
-            $('.winner').text(winnerText); // Who won the round
+            $('.winner').text(winnerText);
 
-            // If the game has been reset set the score immediately
+           
             if(round !== 1) {
 
-                // Set the values once the animation has finished
+              
                 setTimeout(function(){
                     setScore();
                 }, 4000);
 
             } else {
 
-                // Set the values immediately
+             
                 setScore();
                 $('.round').text(round);
 
