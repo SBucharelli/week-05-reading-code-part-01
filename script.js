3b92596a46fec783c5e01829dc8ea3b13210affa
Johnny Rice <jrice@labact.com>
2020-06-29 23:37:45 -0400

    Provide question 02

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 
-        // Set all variables to their base values
-        function resetGame () {
+        // @TODO give this function a name and provide your description of what the function does in your commit message
+        function nameThisFunction(playerChoice, computerChoice, winnerText) {
 
-            playerChoice = "";
-            computerChoice = "";
-            winner = "";
-            round = 1;
-            playerScore = 0;
-            computerScore = 0;
+            $('.player-choice').text(playerChoice);
+            $('.computer-choice').text(computerChoice);
+            $('.winner').text(winnerText);
 
-            setValues();
+            // If the game has been reset set the score immediately
+            if(round !== 1) {
 
-            $('body').removeClass('end-game weapon-chosen');
-            $('.play-again').show();
+                // Set the values once the animation has finished
+                setTimeout(function(){
+                    setScore();
+                }, 4000);
+
+            } else {
+
+                // Set the values immediately
+                setScore();
+                $('.round').text(round);
+
+            }
 
         }
