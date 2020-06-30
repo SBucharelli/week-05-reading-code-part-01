6cd411ffa9f4089dfb85b22e513867f93ed8d3ba
Johnny Rice <jrice@labact.com>
2020-06-30 00:27:48 -0400

    Remove instructions header

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 
-        // @TODO 1) Give this function a descriptive name in place of `nameThisFunction`
-        //          
-        //          Replace `nameThisFunction` with a name you will find more descriptive.
-        //          
-        //          Read the code of the function to come up with a name.
-        //
-        //       2) Rename any variables to match any understanding you make about the code
-        //             
-        //       3) provide your short description of what the function does in your commit message
-        //
-        //        Let's make great commit messages:
-        //        -- remember to read https://chris.beams.io/posts/git-commit/
-        //
-        //        For now the key items from that post to apply are:
-        //        -- Capitalize the commit message
-        //        -- Do not end the message with a period
-        //        -- Use the imperative mood in the subject line
-        //        -- Your message completes this "If applied, this commit will" "Your commit message here"
+      
+        // Decide who wins based on the player & computer choice
         function decideWinner(playerChoice, computerChoice) {
-            // Decide who wins based on the player & computer choice
-
-            var resultClass;
+            let resultClass;
 
             // Who wins, What text is shown & What class is applied to the result screen
             if (playerChoice === computerChoice) {
