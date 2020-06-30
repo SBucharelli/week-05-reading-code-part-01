0d1b1395781fb7573655c86a57589397b5f06536
Johnny Rice <jrice@labact.com>
2020-06-30 00:15:56 -0400

    Provide answer 03

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
         //          Replace `nameThisFunction` with a name you will find more descriptive.
         //          
         //          Read the code of the function to come up with a name.
-        //              
-        //       2) provide your short description of what the function does in your commit message
+        //
+        //       2) Rename any variables to match any understanding you make about the code
+        //             
+        //       3) provide your short description of what the function does in your commit message
         //
         //        Let's make great commit messages:
         //        -- remember to read https://chris.beams.io/posts/git-commit/
@@ -15,26 +17,9 @@
         //        -- Do not end the message with a period
         //        -- Use the imperative mood in the subject line
         //        -- Your message completes this "If applied, this commit will" "Your commit message here"
-        function nameThisFunction(playerChoice, computerChoice, winnerText) {
-
-            $('.player-choice').text(playerChoice);
-            $('.computer-choice').text(computerChoice);
-            $('.winner').text(winnerText);
-
-           
-            if(round !== 1) {
-
-              
-                setTimeout(function(){
-                    setScore();
-                }, 4000);
-
-            } else {
-
-             
-                setScore();
-                $('.round').text(round);
-
-            }
-
+        function computerDecision() {
+            // Decides on whether the computer is playing a, b or c
+            let choices = ['a', 'b', 'c'];
+            let randomChoice = Math.floor(Math.random() * choices.length);
+            return choices[randomChoice];
         }
